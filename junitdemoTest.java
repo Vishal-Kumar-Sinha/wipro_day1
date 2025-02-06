0ecdaad5aed33af04484f3b51afe0fc6fecd04ee
VISHAL KUMAR SINHA <vishalsinha211299@gmail.com>
2025-02-06 13:19:46 +0530

    committed on 06/02/2025

--- junitdemoTest.java
+++ junitdemoTest.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
 
+class getAdd {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
 public class junitdemoTest {
     @Test
     public void getTest() {
@@ -10,4 +16,13 @@ public class junitdemoTest {
         int arr2[]=new int[]{-1,-4,-2,-3};
         assertEquals(-1, c.findMax(arr2));
     }
+    @AfterEach
+    public void getNext() {
+        System.out.println("Going to next test...");
+    }
+    @Test
+    public void addTest() {
+        assertEquals(8,getAdd.add(5, 3),"5+3=8");
+    }
+
 }
